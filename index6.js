78685e20bbbc2b3f62a14aaa760042231f0ce69f
EgorSaienko <114299668+EgorSaienko@users.noreply.github.com>
2022-09-27 17:47:42 +0300

    Add files via upload

---
+++ index6.js
@@ -0,0 +1,26 @@
+console.log("\t\t task1");
+var a = 4;
+console.log("S = ", a * a);
+
+console.log("\t\t task2");
+var number = 67;
+var left = Math.trunc(67/10);
+var right = 67 % 10;
+console.log("Left = ", left);
+console.log("Right = ", right);
+
+console.log("\t\t task3");
+var a = 3;
+var b = 3;
+console.log(a > 2 && b <=3);
+
+console.log("\t\t task4");
+var a = 2;
+var b = 5;
+var c = 5;
+console.log(a < b && b < c);
+
+console.log("\t\t task5");
+var number = 101;
+var perevirka = number / 100;
+console.log(number % 2 != 0 && perevirka >= 1 && perevirka < 10);
