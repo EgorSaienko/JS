33384615ec55fa8cfafe823d0daf190e4061098e
EgorSaienko <114299668+EgorSaienko@users.noreply.github.com>
2022-09-27 17:13:49 +0300

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,15 @@
+console.log("\t\t task1");
+var x;
+x = 10;
+console.log("X = ", x); 
+x = x + 5;
+console.log("New X = ", x);
+
+console.log("\t\t task2");
+var x = 125;
+var y = 450;
+console.log("X = ", x);
+console.log("Y = ", y);
+y = x;
+console.log("X = ", x);
+console.log("Y = ", y);
