2ccc7ceb9a7a5f7d8c55ce6c6f86bb51ad4242cc
EgorSaienko <114299668+EgorSaienko@users.noreply.github.com>
2022-10-03 20:05:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("\t\t task1");
+var arr = [];
+arr = [19, "age", 12 > 5, null];
+console.log(arr.length);
+var variable = prompt("Введіть будь-яке значення");
+arr[4] = variable;
+console.log("Element #5 of the array = ", arr[4]);
